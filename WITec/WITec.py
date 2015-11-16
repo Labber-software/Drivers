803527f36a06b27be308d5178bf73f7b3de6462b
Sebastian Kuhlen <kuhlen@physik.rwth-aachen.de>
2015-11-16 17:17:34 +0100

    New driver for the remote interface of the WITec Control FOUR software
    This driver allows to remote control this spectrometer software.
    Unfortunately, you cannot easily retrieve its data, so with this driver
    Labber acts rather like a remote control than an actual measurement
    software.

---
+++ WITec/WITec.py
@@ -0,0 +1,195 @@
+import InstrumentDriver
+import platform
+import comtypes as com
+import os, sys, inspect, re, math
+
+#Some stuff to import win32gui and win32con from a relative path independent from system wide installations
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+import win32gui
+import win32con
+import WIPfile
+
+class Driver(InstrumentDriver.InstrumentWorker):
+    """ This class wraps the ziPython API"""
+
+    def performOpen(self, options={}):
+        """Perform the operation of opening the instrument connection"""
+        com.CoInitialize()
+        try:
+            self.bucsLib = com.client.GetModule("BasicUniversalCOMServer.tlb")
+        except:
+            raise InstrumentDriver.CommunicationError("Could not connect to WITec Control. Make sure that it is running and that remote access has been granted.")
+        self.rememberWITecWindows()
+        return
+
+    def performClose(self, bError=False, options={}):
+        """Perform the close instrument connection operation"""
+        com.CoUninitialize()
+        return
+
+
+    def performSetValue(self, quant, value, sweepRate=0.0, options={}):
+        """Perform the Set Value instrument operation. This function should
+        return the actual value set by the instrument"""
+        
+        #Some quants are driver settings, that do not require any communication
+        cmd = quant.get_cmd
+        if cmd is None or cmd == "":
+            if quant.name in "CloseWindowsOnTrigger":
+                self.rememberWITecWindows()
+            return value
+    
+        #We reconnect on each call as the connection might have been closed for manual control
+        self.connectToCOM()
+        manip = self.getManipulator(quant)
+        #While LabControl does not distinguish double and int, WITec does. So if writing fails, we have to convert to int
+        try:
+            manip.setValue(value)
+        except:
+            if quant.datatype == quant.DOUBLE:
+                manip.setValue(int(value))
+        return manip.getValue()
+
+
+    def performGetValue(self, quant, options={}):
+        """Perform the Get Value instrument operation"""
+        
+        #Some quants are driver settings, that do not require any communication
+        cmd = quant.get_cmd
+        if cmd is None or cmd == "":
+            return quant.getValue()
+            
+        #We reconnect on each call as the connection might have been closed for manual control
+        self.connectToCOM()
+        if quant.name[:7] == "Trigger":
+            if self.instrCfg.getQuantity('CloseWindowsOnTrigger').getValue():
+                self.closeWITecWindows()
+            lastIndex = self.triggerMeasurement(quant)
+            if quant.name[:14] == "TriggerAndRead":
+                indexManip = self.getNumericManipulator("UserParameters|AutoSaveProject|FileNumber")
+                indexManip.setValue(1)
+                dirManip = self.getStringManipulator("UserParameters|AutoSaveProject|StartDirectory")
+                dirManip.setValue("C:\\")
+                subDirManip = self.getStringManipulator("UserParameters|AutoSaveProject|ExtraDirectory")
+                subDirManip.setValue("Labber-Temp")
+                subDirManip = self.getStringManipulator("UserParameters|AutoSaveProject|FileName")
+                subDirManip.setValue("temp")
+                nextFileManip = self.getStringManipulator("UserParameters|AutoSaveProject|NextFileName")
+                filename = nextFileManip.getValue()
+                triggerManip = self.getTriggerManipulator("UserParameters|AutoSaveProject|StoreProject")
+                triggerManip.OperateTrigger()
+                data = WIPfile.loadWIP(filename)
+                return quant.getTraceDict(data[0], data[1], data[2])
+            else:
+                return lastIndex
+        else:
+            manip = self.getManipulator(quant)
+            return manip.getValue()
+
+    def connectToCOM(self):
+        try:
+            del self.IBUCSCore
+        except:
+            pass
+        self.IBUCSCore = com.client.CreateObject(self.bucsLib.CBUCSCore, None, platform.node(), self.bucsLib.IBUCSCore)
+        IBUCSAccess = self.IBUCSCore.QueryInterface(self.bucsLib.IBUCSAccess)
+        if not IBUCSAccess.RequestWriteAccess(True):
+            raise InstrumentDriver.CommunicationError("Could not get write access for WITec Control. Make sure that remote write access has been granted.")
+        return
+
+    def getGenericManipulator(self, address):
+        return self.IBUCSCore.GetSubSystemDefaultInterface(address)
+        
+    def getStringManipulator(self, address):
+        return self.getGenericManipulator(address).QueryInterface(self.bucsLib.IBUCSString)
+    
+    def getNumericManipulator(self, address):
+        gManip = self.getGenericManipulator(address)
+        try:
+            return gManip.QueryInterface(self.bucsLib.IBUCSInt)
+        except:
+            try:
+                return gManip.QueryInterface(self.bucsLib.IBUCSFloat)
+            except:
+                return None
+    
+    def getBooleanManipulator(self, address):
+        return self.getGenericManipulator(address).QueryInterface(self.bucsLib.IBUCSBool)
+    
+    def getTriggerManipulator(self, address):
+        return self.getGenericManipulator(address).QueryInterface(self.bucsLib.IBUCSTrigger)
+    
+    def getStatusManipulator(self, address):
+        return self.getGenericManipulator(address).QueryInterface(self.bucsLib.IBUCSStatusContainer)
+    
+    def getManipulator(self, quant):
+        if quant.datatype == quant.STRING:
+            return self.getStringManipulator(quant.get_cmd)
+        elif quant.datatype == quant.DOUBLE:
+            return self.getNumericManipulator(quant.get_cmd)
+        elif quant.datatype == quant.BOOLEAN:
+            return self.getBooleanManipulator(quant.get_cmd)
+        return None
+
+    def triggerMeasurement(self, quant):
+        #Handle the file naming within the project
+        nameManip = self.getStringManipulator(quant.get_cmd.rsplit('|',1)[0] + "|Naming|DataName")
+        indexManip = self.getNumericManipulator(quant.get_cmd.rsplit('|',1)[0] + "|Naming|DataNumber")
+        
+        #For some quantities we want to do automatic renaming:
+        name = None
+        if quant.name == "TriggerImageScan":
+            name = self.instrCfg.getQuantity('ImageName').getValue()
+        elif quant.name == "TriggerLineScan":
+            name = self.instrCfg.getQuantity('LineName').getValue()
+        elif quant.name == "TriggerSingleSpectrum":
+            name = self.instrCfg.getQuantity('SpectrumName').getValue()
+        if name is not None:
+            v0 = self.instrCfg.getQuantity('AutoNameValue0').getValue()
+            v1 = self.instrCfg.getQuantity('AutoNameValue1').getValue()
+            v2 = self.instrCfg.getQuantity('AutoNameValue2').getValue()
+            v3 = self.instrCfg.getQuantity('AutoNameValue3').getValue()
+            nameManip.setValue(name.format(v0, v1, v2, v3))
+        
+        #ok, let's go
+        triggerManip = self.getTriggerManipulator(quant.get_cmd)
+        activeManip = self.getStatusManipulator("Status|Software|Sequencers|IsASequencerActive")
+        triggerManip.OperateTrigger()
+        activeManip.update()
+        statInt, statBool = activeManip.GetSingleValueAsInt()
+        while statInt > 0 and statBool == True:
+            self.thread().msleep(500)
+            activeManip.update()
+            statInt, statBool = activeManip.GetSingleValueAsInt()
+        return indexManip.getValue()
+        
+    def rememberWITecWindows(self):
+        self.WITecWindows = []
+        def cb(hwnd, hwnds):
+            if win32gui.IsWindowVisible(hwnd):
+                parent = win32gui.GetWindowText(win32gui.GetWindow(hwnd, win32con.GW_OWNER))
+                self.log("Parent " + parent )
+                if "Control FOUR" in parent:
+                    title = win32gui.GetWindowText(hwnd)
+                    self.log("Remembering " + title )
+                    self.WITecWindows.append(title)
+            return True
+        win32gui.EnumWindows(cb, 0)
+        
+    def closeWITecWindows(self):
+        self.log("close Windows")
+        def cb(hwnd, hwnds):
+            if win32gui.IsWindowVisible(hwnd):
+                parent = win32gui.GetWindowText(win32gui.GetWindow(hwnd, win32con.GW_OWNER))
+                if "Control FOUR" in parent:
+                    title = win32gui.GetWindowText(hwnd)
+                    if (not title in self.WITecWindows) and (not "WITec Control" in title):
+                        self.log("Closing " + title )
+                        win32gui.PostMessage(hwnd, win32con.WM_CLOSE, 0, 0)
+                    else:
+                        self.log("Ignoring " + title )
+            return True
+        win32gui.EnumWindows(cb, 0)
---
+++ WITec/readme.md
@@ -0,0 +1,11 @@
+*WITec Control FOUR remote
+
+##Prerequisite
+
+This driver uses the COM-interface of the WITec Control FOUR software. This interface has to be enabled first or the driver will not work at all. (Please refer to the WITec manuals for this.)
+
+##General notes
+- The auto-close function closes any WITec-window which has not been open when the function has been activated. Therefore, enable the remote in the WITec software before activating this channel.
+- For automatical naming add an auto-name channel as a step channel and enable channel relations to set it to the same value as the main loop. You can then use the number of the auto-name channel as a placeholder in the naming following the Python "format" function. (For example, use {0} as a placeholder for the first auto-name channel)
+- A measurement is triggered by reading one of the trigger-channels.
+- "TriggerAndReadSingleSpectrum" can retrieve the data of a single spectrum into Labber. This is done by telling WITec to save the current project to the HDD and then read the latest spectrum from this file. For this to work, you have to change some setting in "Auto Save": Make sure, that the file is saved using the "extra directory" and that WITec is set to override files. The driver will change the output to C:\Labber-Temp by itself. It is also highly recommended to "Store and clear" to speed up this process. Unfortunately, with this method you will have your data in Labber only.
