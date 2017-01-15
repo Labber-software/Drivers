f5ec0da9fe8cb4907fda056ad71b190fe2e51915
Simon <simongus@gmail.com>
2017-01-14 20:48:11 -0500

    support for all 86x

--- Stanford_SRS86x/SRS_SR86x.py
+++ Stanford_SRS86x/SRS_SR86x.py
@@ -3,7 +3,7 @@
 from VISA_Driver import VISA_Driver
 
 class Driver(VISA_Driver):
-    """ The SRS 830 driver re-implements the VISA driver with extra options"""
+    """ The SRS 86x driver re-implements the VISA driver with extra options"""
 
     def performGetValue(self, quant, options={}):
         """Perform the Get Value instrument operation"""
@@ -11,9 +11,21 @@ class Driver(VISA_Driver):
         name = str(quant.name)
         if name == 'Value':
             # get complex value in one instrument reading
-            sCmd = 'SNAP?0,1'
+            sCmd = 'SNAP? 0,1'
             sAns = self.askAndLog(sCmd).strip()
             lData =  sAns.split(',')
+            
+            #Sometimes, we receive the value twice 
+            #(0.12e-3,4.56e-70.12e-3,4.56e-7 instead of 0.12e-3,4.56e-7)
+            #This is a simple fix:
+            if len(lData) > 2:
+                lData =  sAns[:len(sAns)/2].split(',')
+            #Also, sometimes we receive an additional "-" at the end of a value
+            #(0.12e-3,4.56e-7- instead of 0.12e-3,4.56e-7)
+            #Hence, another simple fix:
+            if lData[1][-1] == "-":
+                lData[1] = ldata[1][:-1]
+                
             # return complex values
             return complex(float(lData[0].strip()), float(lData[1].strip()))
         else:
