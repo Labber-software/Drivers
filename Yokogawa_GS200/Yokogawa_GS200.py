dcb676f86b9a24b28870957ceda505861bf39888
Lab PC2 <simongus@gmail.com>
2017-02-16 15:35:16 -0500

    Rewrote Yoko GS200 driver to follow same notation as 7651

--- Yokogawa_GS200/Yokogawa_GS200.py
+++ Yokogawa_GS200/Yokogawa_GS200.py
@@ -1,124 +1,123 @@
 from VISA_Driver import VISA_Driver
-import InstrumentDriver
 import numpy as np
 
-__version__  = '0.5'
-
-
 class Driver(VISA_Driver):
-    def performSetValue(self, quant, value, sweepRate=0.001, options={}):
-        if quant.name == 'Output':
-            if value:
-                VISA_Driver.writeAndLog(self,'OUTP:STAT ON')
-                return True
-            else:
-                #Sweep source to zero before turning off, to avoid sudden jumps
-                func = self.getValue('Function')
-                if func == 'Voltage':
-                    self.sendValueToOther('Source Voltage Level',0)
-                    while self.checkIfSweeping('Source Voltage Level',options):
-                        self.wait(0.05)
-                elif func == 'Current':
-                    self.sendValueToOther('Source Current Level',0)
-                    while self.checkIfSweeping('Source Current Level',options):
-                        self.wait(0.05)
-                VISA_Driver.writeAndLog(self,'OUTP:STAT OFF')
-                return False
-        elif quant.name == 'Function':
-            if value == 'Voltage':
-                VISA_Driver.writeAndLog(self,'SOUR:FUNC VOLT')
-                return 'Voltage'
-            elif value == 'Current':
-                VISA_Driver.writeAndLog(self,'SOUR:FUNC CURR')
-                return 'Current'
-        elif quant.name == 'Source Current Range' or quant.name == 'Source Voltage Range':
-            VISA_Driver.writeAndLog(self,'SOUR:RANG '+str(value))
+    """ The Yoko driver re-implements the VISA driver with some more options"""
+
+
+    def performOpen(self, options={}):
+        """Perform the operation of opening the instrument connection"""
+        # keep track of sweep state
+        self.is_sweeping = False
+        # start by calling the generic VISA open to make sure we have a connection
+        VISA_Driver.performOpen(self, options=options)
+        # always get function and range: they are essential for correct resolution and sweeping
+        self.readValueFromOther('Function')
+
+
+    def initSetConfig(self):
+        """This function is run before setting values in Set Config.
+        Check if new range is smaller than old, if so first go to new value to
+        avoid zeroing when setting the range with high value"""
+        # get functions and range, first from internal settings (from SetCfg)
+        newFunction = self.getValue('Function')
+        dRangeNew, dMaxNew = self.getMaxValueAndSmallestStep()
+        # get actual settings on instrument by first calling readFromOther
+        oldFunction = self.readValueFromOther('Function')
+        # also read the range settings
+        if oldFunction == 'Voltage':
+            self.readValueFromOther('Range (V)')
+        elif oldFunction == 'Current':
+            self.readValueFromOther('Range (I)')
+        dRangeOld, dMaxOld = self.getMaxValueAndSmallestStep()
+        # check if instrument in different mode ot new range is bigger, if so return
+        if (newFunction != oldFunction) or (dMaxNew > dMaxOld):
+            return
+        # set new value, either voltage or current
+        if newFunction == 'Voltage':
+            quant = self.getQuantity('Voltage')
+        elif newFunction == 'Current':
+            quant = self.getQuantity('Current')
+        # get new value and sweep rate from internal quantity 
+        value = quant.getValue()
+        rate = quant.getSweepRate()
+        # set value here, before changing the range
+        self.sendValueToOther(quant.name, value, sweepRate=rate)
+
+
+    def performSetValue(self, quant, value, sweepRate=0.0, options={}):
+        """Perform the Set Value instrument operation. This function should
+        return the actual value set by the instrument"""
+        # check if set value and in sweep mode
+        if quant.name in ('Voltage', 'Current'):
+            # check limits
+            (dStep, dMax) = self.getMaxValueAndSmallestStep()
+            if abs(value) > dMax:
+                # new value out of range, return error
+                raise Exception('New value (%.6g) is out of range (max = %.6g)' % (value, dMax))
+            # calculate actual value based on smallest step size
+            value = dStep * np.round(value/dStep)
+            # check if sweep mode or output off, if not call generic driver
+            if sweepRate == 0.0 or (not self.getValue('Output')):
+                return VISA_Driver.performSetValue(self, quant, value, 0.0, options=options)
+            # sweep mode, do it here
+            # get old value to find sweep time and step size
+            currValue = self.performGetValue(quant)
+            if value == currValue:
+                # already at the final value, return
+                return value
+            # if sweep range is less than two minimal steps, don't sweep
+            if abs(value-currValue) < 2.5*dStep:
+                return VISA_Driver.performSetValue(self, quant, value, 0.0, options=options)
+            dSweepTime = abs(value-currValue)/sweepRate
+            # don't allow sweep times that are shorter than 0.1 s
+            dSweepTime = max(dSweepTime, 0.1)
+            sSweepTime = '%.1f' % dSweepTime
+            sCmd = '*CLS;:PROG:REP 0;' + \
+                   'SLOP %s;' % sSweepTime + \
+                   'INT %s;'  % sSweepTime + \
+                   'EDIT:STAR;' + \
+                   ':SOUR:LEV %.6E;' % value + \
+                   ':PROG:EDIT:END;' + \
+                   ':PROG:RUN'
+            self.is_sweeping = True
+            VISA_Driver.writeAndLog(self, sCmd)  
+            # return target value
             return value
-        elif quant.name == 'Source Voltage Level' or quant.name == 'Source Current Level':
-            self.log(quant.name+' value passed: '+str(value))
-            self.log('Sweep rate: '+str(sweepRate))
-            #Check current source level
-            initValue = float(self.askAndLog('SOUR:LEV?'))
-            if value == initValue:
-                #if no change, do nothing
-                return float(value)
-            else:
-                #It seems sometimes sweepRate is not passed to this function (e.g. when setting config)
-                # Ensure that this never causes a jump in source levels
-                if quant.name == 'Source Voltage Level':
-                    maxSweepRate = 0.03 #30mV/sec or 1.8V/min
-                else:
-                    maxSweepRate = 0.03 #30mA/sec or 1.8A/min
-                if sweepRate == 0 or sweepRate > maxSweepRate:
-                    sweepRate = maxSweepRate
-                difference = abs(value - initValue)
-                duration = difference/abs(sweepRate)
-                #Note minimum duration is 0.1s
-                duration = max(duration,0.1)
-                cmd1 = 'PROG:REP 0; '               #Program doesn't repeat
-                cmd2 = 'SLOP '+str(duration)+'; '   #Source takes this long to move between values
-                cmd3 = 'INT '+str(duration)+'; '    #This much time between program steps
-                cmd4 = 'EDIT:STAR; '                #Add points to program
-                cmd5 = ':SOUR:LEV '+str(value)+'; ' #Add target point
-                cmd6 = ':PROG:EDIT:END; '           #Stop adding points to program
-                cmd7 = ':PROG:RUN'                  #Run program
-                sCmd = cmd1+cmd2+cmd3+cmd4+cmd5+cmd6+cmd7
-                VISA_Driver.writeAndLog(self,sCmd)  #Combine all the above into one string, then send it
-                return float(value)
         else:
-            self.log('No other quant names triggered: '+quant.name)
-            return VISA_Driver.performSetValue(self,quant,value,options,sweepRate)
-    def performGetValue(self, quant, options={}):
-        if quant.name == 'Output':
-            value = VISA_Driver.askAndLog(self,'OUTP:STAT?')
-            #Value will be 0 (false/off) or 1 (true/on)
-            return int(value)
-        elif quant.name == 'Function':
-            value = VISA_Driver.askAndLog(self,'SOUR:FUNC?')
-            if value == 'VOLT':
-                return 'Voltage'
-            elif value == 'CURR':
-                return 'Current'
-            return value
-        elif quant.name == 'Source Current Range' or quant.name == 'Source Voltage Range':
-            return VISA_Driver.askAndLog(self,'SOUR:RANG?')
-        elif quant.name == 'Source Voltage Level' or quant.name == 'Source Current Level':
-            returnVal = VISA_Driver.askAndLog(self,'SOUR:LEV?')
-            return float(returnVal)
-        else:
-            self.log('Get quantity not listed elsewhere: '+quant.name)
-            return VISA_Driver.performGetValue(self,quant,options)
-    def performStopSweep(self,quant,options={}):
-        VISA_Driver.writeAndLog(self,'PROG:PAUS')
+            # for all other quantities, call the generic VISA driver
+            return VISA_Driver.performSetValue(self, quant, value, sweepRate,
+                                               options=options)
+
+
     def checkIfSweeping(self, quant, options={}):
-        #Can't create quant instances from here, can only pass name of quant
-        #Whereas system
-        if type(quant) == str:
-            name = quant
-        else:
-            name = quant.name
-        if name == 'Source Voltage Level' or name == 'Source Current Level':
-            #If sweeping, can't go into program edit mode
-            #Can't see any other way of checking if program is running
-            VISA_Driver.writeAndLog(self,'*CLS') #Clear any existing errors
-            VISA_Driver.writeAndLog(self,'PROG:EDIT:STAR') #force an error if program running
-            err = VISA_Driver.askAndLog(self,'SYST:ERR?') #Check if there's a 'program running' error
-            #Separate error code and error message
-            #error code = 0 -> no errors
-            errCode = int(err.split(',')[0])
-            if errCode == -284:
-                #There is a 'program running' error, so program must be sweeping
-                return True
-            elif errCode == 103:
-                #There is a 'program being edited error' for some reason. Stop editing
-                self.log("Program didn't stop properly")
-                VISA_Driver.writeAndLog(self,'PROG:EDIT:END')
-                return False
-            else:
-                VISA_Driver.writeAndLog(self,'PROG:EDIT:END')
-                return False
-        else:
-            #Not checking one of the quants that can sweep
-            return False
-if __name__ == '__main__':
-    pass
+        """Check if instrument is sweeping the given quantity"""
+        # check for bit 7 EOP (End of program) im the extended event register
+        status = self.askAndLog(':STAT:EVEN?')
+        # mark as done if bit is set
+        if (int(status) & 128) > 0:
+            self.is_sweeping = False
+        return self.is_sweeping
+
+
+    def getMaxValueAndSmallestStep(self):
+        """Return the resolution, which depends on function (voltage or current)
+        and range settings"""
+        # get function type
+        func = self.getValue('Function')
+        if func == 'Voltage':
+            # get range, voltage
+            dRange = {'30 V': 1E-3, '10 V': 1E-4, '1 V': 1E-5, '100 mV': 1E-6,
+                      '10 mV': 1E-7}
+            dMax = {'30 V': 32.0, '10 V': 12.0, '1 V': 1.2, '100 mV': 0.12,
+                    '10 mV': 0.012}
+            sRange = self.getValue('Range (V)')
+            return (dRange[sRange], dMax[sRange])
+        elif func == 'Current':
+            # get range, current
+            dRange = {'200 mA': 2E-6, '100 mA': 1E-6, '10 mA': 1E-7, '1 mA': 1E-8}
+            dMax = {'200 mA': .2, '100 mA': .12, '10 mA': 0.012, '1 mA': 0.0012}
+            sRange = self.getValue('Range (I)')
+            return (dRange[sRange], dMax[sRange])
+
+
