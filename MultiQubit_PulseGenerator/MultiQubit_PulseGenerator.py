c5720609d1bd80eb0b9dbf9163559f612c95ff0e
Thorvald Larsen <thorvaldw@hotmail.com>
2017-08-24 13:20:10 -0400

    C-phase Echo
    Added pulse sequence for C-phase echo. Also chnaged namings for CZ test pusles.

--- MultiQubit_PulseGenerator/MultiQubit_PulseGenerator.py
+++ MultiQubit_PulseGenerator/MultiQubit_PulseGenerator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from BaseDriver import LabberDriver
-from sequence_builtin import Rabi, CPMG, PulseTrain, CZtest
+from sequence_builtin import Rabi, CPMG, PulseTrain, CZgates, CZecho
 
 import importlib
 
@@ -30,8 +30,10 @@ class Driver(LabberDriver):
                     self.sequence = CPMG()
                 elif value == 'Pulse train':
                     self.sequence = PulseTrain()
-                elif value == 'CZtest':
-                    self.sequence = CZtest()
+                elif value == 'C-phase Pulses':
+                    self.sequence = CZgates()
+                elif value == 'C-phase Echo':
+                    self.sequence = CZecho()
 
         elif quant.name =='Custom python file':
             # for custom python files
